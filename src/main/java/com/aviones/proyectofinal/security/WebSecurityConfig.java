076be97f22a14d35724cc98774f293236fdac47a
Mr-Arzi <goop.robecarlos@gmail.com>
2025-06-18 04:47:59 -0600

    espero y si

--- src/main/java/com/aviones/proyectofinal/security/WebSecurityConfig.java
+++ src/main/java/com/aviones/proyectofinal/security/WebSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -47,9 +48,12 @@ public class WebSecurityConfig {
     }
 
     @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
-        return authConfig.getAuthenticationManager();
-    }
+public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+    return http.getSharedObject(AuthenticationManagerBuilder.class)
+               .authenticationProvider(authenticationProvider())
+               .build();
+}
+
 
     @Bean
     public PasswordEncoder passwordEncoder() {
