9ca5c257a6a4dc9991f2057621493e2612613760
Mr-Arzi <goop.robecarlos@gmail.com>
2025-06-18 02:06:32 -0600

    fix(CORS): allow credentials for localhost2

--- src/main/java/com/aviones/proyectofinal/config/CorsConfig.java
+++ src/main/java/com/aviones/proyectofinal/config/CorsConfig.java
@@ -7,19 +7,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
-
   @Bean
-public WebMvcConfigurer corsConfigurer() {
-  return new WebMvcConfigurer() {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-      registry.addMapping("/**")
-        .allowedOriginPatterns("http://localhost:4200") // 👈 usa allowedOriginPatterns
-        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-        .allowedHeaders("*")
-        .allowCredentials(true)
-        .maxAge(3600);
-    }
-  };
-}
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+          .allowedOriginPatterns("http://localhost:4200")
+          .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+          .allowedHeaders("*")
+          .allowCredentials(true);
+      }
+    };
+  }
 }
